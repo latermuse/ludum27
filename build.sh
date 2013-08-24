651cd0135e5c8fed1a12aeefb503ed72bc1bcaf3
Ron <kormiku@gmail.com>
2013-08-24 12:41:57 +0800

    deleting stuff

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-scp ~/Code/sandbox/ludum27/frontend/* wordpress@latermuse.com:/home/wordpress/public_html/
-scp ~/Code/sandbox/ludum27/backend/* wordpress@latermuse.com:/home/wordpress/backend/
-
-
