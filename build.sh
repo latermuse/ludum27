50f6d285e5c1fd4319f569ddc43f8ab515611b51
Ron <kormiku@gmail.com>
2013-08-24 12:31:44 +0800

    changed some paths

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp ~/Code/sandbox/ludum/frontend/* root@latermuse.com:/home/wordpress/public_html/
-scp ~/Code/sandbox/ludum/backend/* root@latermuse.com:/home/wordpress/backend/
+scp ~/Code/sandbox/ludum27/frontend/* wordpress@latermuse.com:/home/wordpress/public_html/
+scp ~/Code/sandbox/ludum27/backend/* wordpress@latermuse.com:/home/wordpress/backend/
 
 
