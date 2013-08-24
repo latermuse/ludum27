6b99a8e8389a056d9301eb934fef40c0b56f3db7
Ron <kormiku@gmail.com>
2013-08-24 12:27:41 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scp ~/Code/sandbox/ludum/frontend/* root@latermuse.com:/home/wordpress/public_html/
+scp ~/Code/sandbox/ludum/backend/* root@latermuse.com:/home/wordpress/backend/
+
+
---
+++ frontend/index.php
@@ -0,0 +1,11 @@
+<html>
+
+  <head>
+    <title>Latermuse -> 10 Seconds to Glory</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.1/pure-min.css">
+  </head>
+
+  <body></body>
+
+</html>
+
